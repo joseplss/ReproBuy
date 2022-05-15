86cd30b359be89728f739beaf00513efd558645e
joseplss <josepls.v@hotmail.com>
2022-05-15 12:25:16 -0700

    Changed CSS template and made code more readable

--- reproBuy/wwwroot/js/toastr.js
+++ reproBuy/wwwroot/js/toastr.js
@@ -144,3 +144,24 @@ function toastrTrash() {
     }
     toastr["info"]("Changed your mind? No worries!", "Cart has been cleared")
 }
+//FOR AZURE ONLY
+function azureOnly() {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+    toastr["info"]("For purpose of Azure, adding an item is disabled!")
+}
